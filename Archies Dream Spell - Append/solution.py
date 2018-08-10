57b278fc4fc33c9a849fcd27c949f9a25a80b3b7
tanmay jain <tanmayjain.cs@gmail.com>
2018-08-10 09:55:57 +0530

    appended space to dict values

--- Archies Dream Spell - Append/solution.py
+++ Archies Dream Spell - Append/solution.py
@@ -21,15 +21,15 @@ numInWordsDict = {
   '80': 'eighty ',
   '90': 'ninety ',
 
-  '11': 'eleven',
-  '12': 'twelve',
-  '13': 'thirteen',
-  '14': 'fourteen',
-  '15': 'fifteen',
-  '16': 'sixteen',
-  '17': 'seventeen',
-  '18': 'eighteen',
-  '19': 'nineteen'
+  '11': 'eleven ',
+  '12': 'twelve ',
+  '13': 'thirteen ',
+  '14': 'fourteen ',
+  '15': 'fifteen ',
+  '16': 'sixteen ',
+  '17': 'seventeen ',
+  '18': 'eighteen ',
+  '19': 'nineteen '
 }
 def num2words(number):
         number = str(number)
