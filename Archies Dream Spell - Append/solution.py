83d18d4558955ac062b6299cc1cd8faaefcc1569
tanmay jain <tanmayjain.cs@gmail.com>
2018-08-10 09:54:51 +0530

    Update solution.py
    -- improvements
    added support for 5 digit numbers
    now number to word conversion has its own function
    -- fix :
    when input was single digit number, number to word conversion was not correct like for when input number was 7 it was printing seventy seven

--- Archies Dream Spell - Append/solution.py
+++ Archies Dream Spell - Append/solution.py
@@ -31,26 +31,43 @@ numInWordsDict = {
   '18': 'eighteen',
   '19': 'nineteen'
 }
+def num2words(number):
+        number = str(number)
+        length = len(number)     
+        inWords =''  
+        if length>4:
+            if number[:2] in numInWordsDict:
+                inWords +=numInWordsDict[number[:2]]  +'thousand '
+            else:
+                inWords +=numInWordsDict[number[0]+'0'] + numInWordsDict[number[1]] +'thousand '
+                
+              
+        inWords += numInWordsDict[number[length-4]]+'thousand ' if length== 4 and number[length-4] != '0' else ''
 
+        inWords += numInWordsDict[number[length-3]]+'hundred ' if length > 2 and number[length-3] != '0' else ''
+
+        lastTwoDigits = 0
+        if length > 1:
+            lastTwoDigits = int(number[-2]+'0')  
+            lastTwoDigits += int(number[-1])
+            if lastTwoDigits > 9 and lastTwoDigits < 20:
+                inWords += numInWordsDict[str(lastTwoDigits)] #direct key exist # 19
+            else:
+                inWords += numInWordsDict[number[-2]+'0'] + numInWordsDict[number[-1]] # 20 + 3
+            
+        else:
+            inWords = numInWordsDict[number[0]]
+            inWords = inWords.rstrip()
+
+
+        return(inWords) # inWords.title()
+
+    return(in_wds)
+  
 for i in range(10):
     number = input()
-    print(number)
-            
-    length = len(number)     
-    inWords =''  
-    inWords += numInWordsDict[number[length-4]]+'thousand ' if length>3 and number[length-4] != '0' else ''
-    inWords += numInWordsDict[number[length-3]]+'hundred ' if length >2 and number[length-3] != '0' else ''
-    
-    lastTwoDigits = int(number[length-2]+'0') + int(number[length-1])
-    
-    if lastTwoDigits > 10 and lastTwoDigits <20 and length >1:
-        inWords += numInWordsDict[str(lastTwoDigits)]
-    else:
-        inWords += numInWordsDict[number[length-2]+'0']
-        inWords += numInWordsDict[number[length-1]]
-        inWords = inWords.rstrip()
-    print(inWords)
-    
+    print(number,'\n',num2words(number))
+       
     pwd = ''
     for w in inWords.split(' '):
         pwd+=str(len(w))
